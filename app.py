c2c6652f6c2b9cd4bbf013ca408b372089933cdb
skllzrmy.tez <joe@poundfit.com>
2023-11-02 22:10:28 -0700

    🔧 fix(app.py): add support for different file types and validate file type before unfucking to prevent errors 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support different file types 🔧 fix(app.py): change file_content retrieval method from JSON payload to form data to support

--- app.py
+++ app.py
@@ -1,98 +1,58 @@
 from flask import Flask, request, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_cors import CORS
 from unfucker.unfucker import Unfucker
 import logging
 import tempfile
-import os
+import re
 
 app = Flask(__name__)
-
-# Initialize Limiter
-limiter = Limiter(
-    key_func=get_remote_address,
-    app=app,
-    default_limits=["20 per minute"],
-)
-
-# Configure logging
+CORS(app)
+limiter = Limiter(key_func=get_remote_address, app=app, default_limits=["20 per minute"])
 logging.basicConfig(filename='api.log', level=logging.INFO)
+ALLOWED_FILE_TYPES = {"txt", "json", "xml"}
 
 @app.route("/unfuck", methods=["POST"])
 @limiter.limit("20 per minute")
 def unfuck_file():
-    """
-    Endpoint to unfuck a file's content. It expects a POST request with JSON payload.
-
-    The JSON should contain:
-    - file_content (str): The content of the file to be unfucked.
-    - max_iterations (int, optional): Maximum iterations to attempt unfucking. Defaults to 10.
-
-    Returns:
-    - On success: JSON object containing 'success': True and 'unfucked_content': <content>
-    - On failure: JSON object containing 'success': False and 'error': <error message>
-
-    Raises HTTP 400 for bad requests and HTTP 429 for rate limit exceeded.
-    """
-    try:
-        data = request.get_json()
-    except:
-        return bad_request("Your request is empty, like your soul")
-
-    file_content = data.get("file_content")
-    max_iterations = data.get("max_iterations", 10)
-
-    if not all([file_content]):
+    file_content = request.form.get("file_content")
+    if file_content is None:
         return bad_request("Missing required field: file_content")
 
-    with tempfile.NamedTemporaryFile(suffix=f"_unfuck_temp", delete=False) as temp:
-        temp_file_path = temp.name
-        temp.write(file_content.encode())
+    max_iterations = request.form.get("max_iterations", 10, type=int)
+    file_type = request.form.get("file_type", "txt").lower()
+    if file_type not in ALLOWED_FILE_TYPES:
+        return bad_request("Invalid file type. Allowed types are: txt, json, xml")
 
-    unfucker = Unfucker(temp_file_path, max_iterations)
-    fixed_content, error = unfucker.unfuck()
+    file_extension = re.sub(r'[^a-zA-Z]', '', file_type)
+    try:
+        with tempfile.NamedTemporaryFile(suffix=f"_unfuck_temp.{file_extension}", delete=True) as temp:
+            temp.write(file_content.encode())
+            temp.flush()
+            unfucker = Unfucker(temp.name, file_extension, max_iterations)
+            fixed_content, error = unfucker.unfuck()
 
-    os.remove(temp_file_path)  # Deleting the tempfile
-
-    if fixed_content:
-        logging.info("Successfully unfucked a file")
-        return jsonify({"success": True, "unfucked_content": fixed_content}), 200
-
-    return bad_request(f"Failed to unfuck. Maybe it's you, not the file. Reason: {error}")
+        if fixed_content:
+            logging.info(f"Successfully unfucked {temp.name}")
+            return jsonify({"success": True, "unfucked_content": fixed_content, "file_type": file_extension}), 200
+        return bad_request(f"Failed to unfuck. Maybe it's you, not the file. Reason: {error}")
+    except Exception as e:
+        logging.exception("Error during unfuck operation")
+        return internal_error(e)
 
 @app.errorhandler(400)
-def bad_request(e):
-    """
-    Error handler for HTTP 400 Bad Request.
-
-    Args:
-    e (str): Error message to be included in the response.
-
-    Returns:
-    JSON object containing 'success': False and 'error': <error message>, HTTP status 400
-    """
-    logging.warning(f"Bad request: {e}")
-    return jsonify({"success": False, "error": f"Your request is fucked: {e}"}), 400
+def bad_request(error):
+    logging.warning(f"Bad request: {error}")
+    return jsonify({"success": False, "error": f"Your request is fucked: {error}"}), 400
 
 @app.errorhandler(429)
 def rate_limit_exceeded(e):
-    """
-    Error handler for HTTP 429 Too Many Requests (rate limit exceeded).
-
-    Returns:
-    JSON object containing 'success': False and 'error': 'Too fast, cowboy. Try again in a minute', HTTP status 429
-    """
     logging.warning("Rate limit exceeded")
     return jsonify({"success": False, "error": "Too fast, cowboy. Try again in a minute"}), 429
 
 @app.errorhandler(500)
 def internal_error(e):
-    """
-    Error handler for HTTP 500 Internal Server Error.
-
-    Returns:
-    JSON object containing 'success': False and 'error': 'The server is pretty fucked', HTTP status 500
-    """
     logging.exception("Internal error")
     return jsonify({"success": False, "error": "The server is pretty fucked"}), 500
 
